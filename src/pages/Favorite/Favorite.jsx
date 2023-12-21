93036dcdec35a0a63646d1ec294e5a54100e1129
Mehedi hasan ronm <hassan.mehedi2026@gmail.com>
2023-12-21 23:33:44 +0600

    projecct done

--- src/pages/Favorite/Favorite.jsx
+++ src/pages/Favorite/Favorite.jsx
@@ -3,22 +3,24 @@ import FavoriteCart from "./FavoriteCart";
 
 
 const Favorite = () => {
-    const [favorites,setFavorites]=useState([]);
-    const [isEmpty , setIsEmpty]=useState(false);
+    const [favorites, setFavorites] = useState([]);
+    const [isEmpty, setIsEmpty] = useState(false);
 
-    useEffect(()=>{
+    const [isShow, setShow] = useState(false);
+
+    useEffect(() => {
         const favoriteItems = JSON.parse(localStorage.getItem('favorite'));
-        if(favoriteItems){
+        if (favoriteItems) {
             setFavorites(favoriteItems);
         }
-        else{
+        else {
             setIsEmpty("no items found");
             setFavorites(favoriteItems);
 
         }
-    },[])
+    }, [])
 
-    const handleDelete =()=>{
+    const handleDelete = () => {
         localStorage.clear();
         setFavorites([]);
         setIsEmpty("no items found");
@@ -26,19 +28,22 @@ const Favorite = () => {
     }
 
     return (
-        <div>
-            { isEmpty ? <div className="flex items-center justify-center">{isEmpty}</div> : <div>
+        <div className="py-20">
+            {isEmpty ? <div className="flex items-center justify-center">{isEmpty}</div> : <div>
 
-            <div>
-            {favorites.length>0 && <button onClick={handleDelete} className="flex items-center">Delete all </button>}
-         
-                {
-                    favorites.map(phone => <FavoriteCart key={phone.id} phone={phone}></FavoriteCart>)
-                }
-            </div>
+                <div>
+                    {favorites.length > 0 && <button onClick={handleDelete} className="flex items-center">Delete all </button>}
+
+                    {
+                        isShow ? <div> {favorites.map(phone => <FavoriteCart key={phone.id} phone={phone}></FavoriteCart>)}</div>
+                            : <div>{favorites.slice(0, 2).map(phone => <FavoriteCart key={phone.id} phone={phone}></FavoriteCart>)}</div>
+                    }
+                </div>
+                {favorites.length > 2 && <button onClick={() => setShow(!isShow)} className="flex justify-center  mx-auto bg-green-400 font-extrabold "
+                    >{isShow ? "Show less" : "Show more" }</button>}
             </div>
             }
-            
+
         </div>
     );
 };
